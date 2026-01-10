6070549f8df1f95960f54d86d528317bf61347bb
Quentin Smith <quentinsmith3006@gmail.com>
2026-01-10 16:36:53 -0500

    Set up http requests.

--- src/main/java/com/github/quentinsmith3006afk/chessapiapp/httptools/HttpTools.java
+++ src/main/java/com/github/quentinsmith3006afk/chessapiapp/httptools/HttpTools.java
@@ -1,5 +1,52 @@
 package com.github.quentinsmith3006afk.chessapiapp.httptools;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpResponse;
+import java.net.http.HttpRequest;
+import java.net.http.HttpClient;
 
 public class HttpTools {
+    public static final String START = "";
+
+    public static Gson GSON = new GsonBuilder()
+            .setPrettyPrinting()
+            .create();
+
+    public static HttpClient httpClient = HttpClient.newBuilder().build();
+
+    public static String request(String endPointUrl) {
+        try {
+            HttpRequest httpRequest = HttpRequest.newBuilder()
+                    .uri(new URI(START + endPointUrl))
+                    .build();
+
+            String json = request(httpRequest);
+
+
+
+
+        } catch (URISyntaxException urise) {
+            urise.printStackTrace();
+        }
+
+        return "";
+    }
+
+    public static String request(HttpRequest httpRequest) {
+        String json = "";
+        try {
+            HttpResponse<String> response = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+            json = response.body();
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return json;
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.github.quentinsmith3006afk.chessapiapp {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.google.gson;
+    requires java.net.http;
 
     opens com.github.quentinsmith3006afk.chessapiapp;
     exports com.github.quentinsmith3006afk.chessapiapp;
